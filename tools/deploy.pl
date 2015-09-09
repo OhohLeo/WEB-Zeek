b8f7c819f79ef2aebaf251a0a0f6f532f64d6bc0
Léo Martin <lmartin@ijenko.com>
2015-09-09 15:32:03 +0200

    tools/deploy.pl: mise en place d'un outil de déploiement

--- tools/deploy.pl
+++ tools/deploy.pl
@@ -2,10 +2,16 @@
 use strict;
 use warnings;
 
+use File::Path qw(make_path remove_tree);
 use File::Basename;
+use File::Copy;
+
 use Getopt::Long;
 use Net::FTP;
 use Cwd;
+
+use CSS::Minifier;
+use JavaScript::Minifier;
 
 use feature 'say';
 
@@ -18,7 +24,6 @@ GetOptions(
     'host|t=s'       => \$host,
     'login|l=s'      => \$login,
     'password|p=s'   => \$password,
-    'directory|d=s'  => \$directory,
     'input|i=s'      => \@input,
     'output|o=s'     => \$output,
     'force|f'        => \$force,
@@ -27,13 +32,12 @@ GetOptions(
 sub display_help
 {
     die <<END;
-usage : deploy.pl {options} [ directory / file ]
+usage : deploy.pl {options}
                   -a [see|update|clean]
                   -t HOST NAME
                   -l LOGIN NAME
                   -p PASSWORD
-                  -d DIRECTORY
-                  -i INPUT
+                  -i INPUT [ directory / file ]
                   -o OUTPUT
                   -f FORCE
                   -h this help
@@ -43,9 +47,8 @@ END
 display_help() if $help;
 
 # action by default is update
-$action //= 'update';
+$action //= 'build';
 $directory //= cwd();
-$host //= 'ftpperso.free.fr';
 
 sub read_directory
 {
@@ -62,13 +65,16 @@ sub read_directory
 	my $filename = $_;
 	my $url = "$directory/$filename";
 
-	if (-d $url
-            and not exists($FORBIDDEN{$filename})
-            and substr($filename, 0, 1) ne ".")
+        # Remove hidden file & readme, license & md files
+        next if (substr($filename, 0, 1) eq "."
+                 or lc($filename) ~~ [ qw(readme license) ]
+                 or $filename =~ /\.(md|svg)$/i);
+
+	if (-d $url and not exists($FORBIDDEN{$filename}))
 	{
 	    read_directory($url, $files);
 	}
-	elsif (-f $url) # and $filename =~ /\.(html|php|js|css|ini|png)+$/)
+	elsif (-f $url)
 	{
 	    next if $filename =~ /^(external_|internal_)+/;
 
@@ -133,194 +139,243 @@ if ($action eq 'see')
     exit;
 }
 
-if ($action eq 'clean')
+say "cleaning zeek";
+
+my $dst = "$directory/build";
+
+# Clean build directory
+remove_tree $dst if -d $dst;
+
+exit if $action eq 'clean';
+
+say "building zeek";
+
+# Create new directory
+mkdir $dst;
+
+while (my($src_directory, $src_files) = each %files)
 {
+    my $dst_directory =  $dst . substr($src_directory, length($directory));
+
+    # Create all path
+    make_path $dst_directory;
+
+    # Copy all files
+    foreach my $file (@$src_files)
+    {
+        if ($file =~ /\.(js|css)$/i)
+        {
+            my $ext = lc($1);
+
+            my($in, $out);
+
+            open($in, "$src_directory/$file") or die;
+            open($out, ">$dst_directory/$file") or die;
+
+            if ($ext eq 'js')
+            {
+                JavaScript::Minifier::minify(input => $in, outfile => $out);
+            }
+            elsif ($ext eq 'css')
+            {
+                CSS::Minifier::minify(input => $in, outfile => $out);
+            }
+
+            close($in);
+            close($out);
+        }
+        else
+        {
+            copy("$src_directory/$file", "$dst_directory/$file");
+        }
+    }
 }
+
 
 unless ($action eq 'update')
 {
     die "unknown action '$action'!\n";
 }
 
-unless (defined $login and defined $password and defined $host) {
-    display_help();
-}
+# unless (defined $login and defined $password and defined $host) {
+#     display_help();
+# }
 
-# we handle specific behavior concerning the host
-my($action_before, $action_after);
+# # we handle specific behavior concerning the host
+# my($action_before, $action_after);
 
-if ($host eq 'ftpperso.free.fr')
-{
-    my $filename = '.htaccess';
+# if ($host eq 'ftpperso.free.fr')
+# {
+#     my $filename = '.htaccess';
 
-    $action_before = sub {
-	# we create the file
-	my $filehandle;
-	open($filehandle, '>', "$directory/$filename");
-	print {$filehandle} <<END;
-php56 1
-END
+#     $action_before = sub {
+# 	# we create the file
+# 	my $filehandle;
+# 	open($filehandle, '>', "$directory/$filename");
+# 	print {$filehandle} <<END;
+# php56 1
+# END
 
-	close($filehandle);
+# 	close($filehandle);
 
-	# we add the file to the list
-	push(@{$files{$directory}}, $filename);
+# 	# we add the file to the list
+# 	push(@{$files{$directory}}, $filename);
 
-	# we add the json to the list
-	$files{"$directory/extends"} = [ 'json.php' ];
+# 	# we add the json to the list
+# 	$files{"$directory/extends"} = [ 'json.php' ];
 
-	# we use /var/config_free.ini file
-	replace_link("$directory/config.ini",
-		     "$directory/var/config_free.ini");
+# 	# we use /var/config_free.ini file
+# 	replace_link("$directory/config.ini",
+# 		     "$directory/var/config_free.ini");
 
-	# we add the sessions directory
-	mkdir("$directory/sessions");
+# 	# we add the sessions directory
+# 	mkdir("$directory/sessions");
 
-	# we add the sessions to the list
-	$files{"$directory/sessions"} = [];
-    };
+# 	# we add the sessions to the list
+# 	$files{"$directory/sessions"} = [];
+#     };
 
-    $action_after = sub {
-	# we remove the file
-	unlink("$directory/$filename");
+#     $action_after = sub {
+# 	# we remove the file
+# 	unlink("$directory/$filename");
 
-	# we remove the sessions directory
-	rmdir("$directory/sessions");
+# 	# we remove the sessions directory
+# 	rmdir("$directory/sessions");
 
-	# we use config file again
-	replace_link("$directory/config.ini",
-		     "$directory/var/config.ini");
-    };
-}
-elsif ($host eq 'divebartheband.com')
-{
-    my $filename = '.htaccess';
+# 	# we use config file again
+# 	replace_link("$directory/config.ini",
+# 		     "$directory/var/config.ini");
+#     };
+# }
+# elsif ($host eq 'divebartheband.com')
+# {
+#     my $filename = '.htaccess';
 
-    $action_before = sub {
-	# we create the file
-	my $filehandle;
-	open($filehandle, '>', "$directory/$filename");
-	print {$filehandle} <<END;
-AddType x-mapp-php5.5 .php
-AddHandler x-mapp-php5.5 .php
+#     $action_before = sub {
+# 	# we create the file
+# 	my $filehandle;
+# 	open($filehandle, '>', "$directory/$filename");
+# 	print {$filehandle} <<END;
+# AddType x-mapp-php5.5 .php
+# AddHandler x-mapp-php5.5 .php
 
-RewriteEngine on
-RewriteCond %{REQUEST_URI} !^/zeek
-RewriteRule ^(.*)$ /zeek/projects/1/DEPLOY/\$1 [L]
+# RewriteEngine on
+# RewriteCond %{REQUEST_URI} !^/zeek
+# RewriteRule ^(.*)$ /zeek/projects/1/DEPLOY/\$1 [L]
 
-END
+# END
 
-        chmod(0604, $filehandle);
-	close($filehandle);
+#         chmod(0604, $filehandle);
+# 	close($filehandle);
 
-	# we add the file to the list
-        $files{"*$directory"} = [];
-        push(@{$files{"*$directory"}}, $filename);
+# 	# we add the file to the list
+#         $files{"*$directory"} = [];
+#         push(@{$files{"*$directory"}}, $filename);
 
-	# we add the sessions directory
-	mkdir("$directory/sessions");
+# 	# we add the sessions directory
+# 	mkdir("$directory/sessions");
 
-	# we use /var/config_free.ini file
-	replace_link("$directory/config.ini",
-		     "$directory/var/config_1and1.ini");
+# 	# we use /var/config_free.ini file
+# 	replace_link("$directory/config.ini",
+# 		     "$directory/var/config_1and1.ini");
 
-	# we add the sessions to the list
-	$files{"*$directory/sessions"} = [];
-    };
+# 	# we add the sessions to the list
+# 	$files{"*$directory/sessions"} = [];
+#     };
 
-    $action_after = sub {
-	# we remove the file
-	unlink("$directory/$filename");
+#     $action_after = sub {
+# 	# we remove the file
+# 	unlink("$directory/$filename");
 
-	# we remove the sessions directory
-	rmdir("$directory/sessions");
+# 	# we remove the sessions directory
+# 	rmdir("$directory/sessions");
 
-        # we use config file again
-	replace_link("$directory/config.ini",
-		     "$directory/var/config.ini");
-    };
-}
+#         # we use config file again
+# 	replace_link("$directory/config.ini",
+# 		     "$directory/var/config.ini");
+#     };
+# }
 
-# we try to establish connection
-my $ftp = Net::FTP->new($host, Passive => 1)
-    or die "Cannot connect to '$host': $@";
+# # we try to establish connection
+# my $ftp = Net::FTP->new($host, Passive => 1)
+#     or die "Cannot connect to '$host': $@";
 
-# we try to login
-$ftp->login($login, $password)
-    or die "Cannot login " . $ftp->message;
+# # we try to login
+# $ftp->login($login, $password)
+#     or die "Cannot login " . $ftp->message;
 
-say("connected to $host!");
+# say("connected to $host!");
 
-# Place both servers into the correct transfer mode.
-# In this case I'm using ASCII.
-$ftp->ascii() or die  "Can't set ASCII mode: $!";
+# # Place both servers into the correct transfer mode.
+# # In this case I'm using ASCII.
+# $ftp->ascii() or die  "Can't set ASCII mode: $!";
 
-# we link the default script file to external
-foreach my $replace (qw(scripts header))
-{
-    replace_link("$directory/default/$replace.php",
-		 "$directory/default/external_$replace.php");
-}
+# # we link the default script file to external
+# foreach my $replace (qw(scripts header))
+# {
+#     replace_link("$directory/default/$replace.php",
+# 		 "$directory/default/external_$replace.php");
+# }
 
-if (defined $action_before) {
-    $action_before->();
-}
+# if (defined $action_before) {
+#     $action_before->();
+# }
 
-my $size = length($directory);
+# my $size = length($directory);
 
-while (my($cwd, $files) = each %files)
-{
-    my $orig_dst;
+# while (my($cwd, $files) = each %files)
+# {
+#     my $orig_dst;
 
-    if (substr($cwd, 0, 1) eq '*')
-    {
-        $cwd = substr($cwd, 1);
-        $orig_dst = 1;
-    }
+#     if (substr($cwd, 0, 1) eq '*')
+#     {
+#         $cwd = substr($cwd, 1);
+#         $orig_dst = 1;
+#     }
 
-    my $relative_dst = substr($cwd, $size);
-    my $full_dst = $relative_dst;
+#     my $relative_dst = substr($cwd, $size);
+#     my $full_dst = $relative_dst;
 
-    if ($output && not $orig_dst)
-    {
-        $full_dst = "/$output$relative_dst";
-    }
+#     if ($output && not $orig_dst)
+#     {
+#         $full_dst = "/$output$relative_dst";
+#     }
 
-    if ($full_dst eq '')
-    {
-	$full_dst = '/';
-    }
-    else
-    {
-	# we create the directory
-	$ftp->mkdir($full_dst, 1);
-    }
+#     if ($full_dst eq '')
+#     {
+# 	$full_dst = '/';
+#     }
+#     else
+#     {
+# 	# we create the directory
+# 	$ftp->mkdir($full_dst, 1);
+#     }
 
-    say "change directory '$full_dst'";
-    $ftp->cwd($full_dst)
-	or die "Cannot change working directory ", $ftp->message;
+#     say "change directory '$full_dst'";
+#     $ftp->cwd($full_dst)
+# 	or die "Cannot change working directory ", $ftp->message;
 
-    # we write the file
-    foreach my $file (@$files)
-    {
-        # we set binary mode
-        $ftp->binary;
-        $ftp->put("$directory/$relative_dst/$file");
-        say "send $file";
-    }
-}
+#     # we write the file
+#     foreach my $file (@$files)
+#     {
+#         # we set binary mode
+#         $ftp->binary;
+#         $ftp->put("$directory/$relative_dst/$file");
+#         say "send $file";
+#     }
+# }
 
-# we link the default script file to internal
-foreach my $replace (qw(scripts header))
-{
-    replace_link("$directory/default/$replace.php",
-		 "$directory/default/internal_$replace.php");
-}
+# # we link the default script file to internal
+# foreach my $replace (qw(scripts header))
+# {
+#     replace_link("$directory/default/$replace.php",
+# 		 "$directory/default/internal_$replace.php");
+# }
 
-if (defined $action_after) {
-    $action_after->();
-}
+# if (defined $action_after) {
+#     $action_after->();
+# }
 
-# we close connection
-$ftp->close;
-$ftp->quit;
+# # we close connection
+# $ftp->close;
+# $ftp->quit;
