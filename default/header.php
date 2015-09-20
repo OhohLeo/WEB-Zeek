f96707bddd428d79db9827c7241e15d70821cf33
Léo Martin <lmartin.leo@gmail.com>
2015-09-20 21:18:22 +0200

    modify default/header & default/scripts

--- default/header.php
+++ default/header.php
@@ -1,1 +1,11 @@
-/home/leo/git/WEB-Zeek/default/internal_header.php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zeek</title>
+    <link rel="stylesheet" href="css/zeek.css">
+  </head>
+
+  <body>
